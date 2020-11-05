dcd1b5cc6be519b437827d23fed72442f878062b
fhchl <fhchl@users.noreply.github.com>
2020-11-05 19:55:55 +0100

    list works!

--- ltbl.c
+++ ltbl.c
@@ -14,14 +14,12 @@ typedef struct _ltbl {
   struct RGBLedMatrixOptions options;
   struct RGBLedMatrix *matrix;
   struct LedCanvas *offscreen_canvas;
-  int width, height;
-  int x, y, i;
+  int height, width;
 } t_ltbl;
-
 
 void ltbl_free(t_ltbl *x)
 {
-  //led_matrix_delete(x->matrix); 
+  led_matrix_delete(x->matrix); 
 }
 
 void str_replace_char(char *s, char c, char r)
@@ -30,11 +28,25 @@ void str_replace_char(char *s, char c, char r)
   for (size_t i=0; i<n; i++) if (s[i] == c) s[i] = r;
 }
 
+void ltbl_destroy(t_ltbl *x)
+{
+  // do nothing without initizialisation
+  if (x->matrix == NULL)
+  {
+    error("Matrix not initialized.");
+    return;
+  }
+  led_matrix_delete(x->matrix);
+  x->matrix = NULL;
+  if (x->matrix != NULL) printf("matrix not null");
+}
 
 void ltbl_init(t_ltbl *x, t_symbol *s, int argc, t_atom *argv)
 {
-  char **argv_char = malloc(sizeof(char*) * argc);
+  // tear down old matrix before creating new
+  ltbl_destroy(x);  
   
+  char **argv_char = malloc(sizeof(char*) * (argc + 1));
   for (int i=0; i<argc; i++)
   {
     //~ post(strdup(atom_getsymbol(argv+i)->s_name));
@@ -46,38 +58,113 @@ void ltbl_init(t_ltbl *x, t_symbol *s, int argc, t_atom *argv)
     }
     str_replace_char(argv_char[i], '!', ';') ;
     post(argv_char[i]);
+    fprintf(stderr, "%s\n", argv_char[i]);
   }
   
+  // dont drop root priviliges after initializig, so one can initialize
+  // more matrices
+  argv_char[argc] = "--led-no-drop-privs";
+  post(argv_char[argc]);
+  argc++;
+  
   memset(&x->options, 0, sizeof(x->options));
-   
-  /* This supports all the led commandline options. Try --led-help */
   x->matrix = led_matrix_create_from_options(&x->options, &argc, &argv_char);
-  //~ if (x->matrix == NULL) 
-  //~ {
-    //~ error("Could not create matrix :/");
-    //~ return;
-  //~ }
+  if (x->matrix == NULL) 
+  {
+    error("Could not create matrix :/");
+    return;
+  }
 
-  //~ /* Let's do an example with double-buffering. We create one extra
-   //~ * buffer onto which we draw, which is then swapped on each refresh.
-   //~ * This is typically a good aproach for animations and such.
-   //~ */
-  //~ x->offscreen_canvas = led_matrix_create_offscreen_canvas(x->matrix);
+  x->offscreen_canvas = led_matrix_create_offscreen_canvas(x->matrix);
 
-  //~ led_canvas_get_size(x->offscreen_canvas, &x->width, &x->height);
+  led_canvas_get_size(x->offscreen_canvas, &x->width, &x->height);
 
-  //~ fprintf(stderr, "Size: %dx%d. Hardware gpio mapping: %s\n",
-          //~ x->width, x->height, x->options.hardware_mapping);
+  fprintf(stderr, "Size: %dx%d. Hardware gpio mapping: %s\n",
+          x->width, x->height, x->options.hardware_mapping);
 }
 
-void ltbl_list(t_ltbl *x, t_symbol *s, int argc, t_atom *argv) {}
+void ltbl_list(t_ltbl *x, t_symbol *s, int argc, t_atom *argv)
+{
+  // do nothing without initizialisation
+  if (x->matrix == NULL)
+  {
+    error("Matrix not initialized.");
+    return;
+  }
+  
+  if (argc != x->width * x->height * 3)
+  {
+    pd_error(x, "Wrong number of pixels: %i != %i x %i x 3", argc, x->width, x->height);
+  }
+  
+  int i;
+  uint8_t r,g,b;
+  for (int ix=0; ix<x->width; ix++)
+  { 
+    for (int iy=0; iy<x->height; iy++)
+    {
+      i = 3 * (ix + iy * x->width);
+      fprintf(stderr, "ix: %i iy: %i i: %i\n", ix, iy, i);
+      r = atom_getint(argv + i);
+      g = atom_getint(argv + i + 1);
+      b = atom_getint(argv + i + 2);
+      
+      led_canvas_set_pixel(x->offscreen_canvas, ix, iy, r, g, b);
+    }
+  }
+  x->offscreen_canvas = led_matrix_swap_on_vsync(x->matrix, x->offscreen_canvas);
+}
+
+// draw test image
+void ltbl_test(t_ltbl *x)
+{   
+    // do nothing without initizialisation
+    if (x->matrix == NULL)
+    {
+      error("Matrix not initialized.");
+      return;
+    }
+    
+    led_canvas_clear(x->offscreen_canvas);
+    
+    led_canvas_set_pixel(x->offscreen_canvas, 1, 0, 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, 0, 0, 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, 0, 1, 255, 255, 255);
+    
+    led_canvas_set_pixel(x->offscreen_canvas, x->width - 1, 0, 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, x->width - 1, 1, 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, x->width - 2, 0, 255, 255, 255);
+        
+    led_canvas_set_pixel(x->offscreen_canvas, 0, x->height - 1, 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, 1, x->height - 1, 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, 0, x->height - 2, 255, 255, 255);
+    
+    led_canvas_set_pixel(x->offscreen_canvas, x->width - 1 , x->height - 1 , 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, x->width - 2 , x->height - 1 , 255, 255, 255);
+    led_canvas_set_pixel(x->offscreen_canvas, x->width - 1 , x->height - 2 , 255, 255, 255);
+
+    x->offscreen_canvas = led_matrix_swap_on_vsync(x->matrix, x->offscreen_canvas);
+}
+
+void ltbl_clear(t_ltbl *x)
+{
+  // do nothing without initizialisation
+  if (x->matrix == NULL)
+  {
+    error("Matrix not initialized.");
+    return;
+  }
+  led_canvas_clear(x->offscreen_canvas);
+  x->offscreen_canvas = led_matrix_swap_on_vsync(x->matrix, x->offscreen_canvas);
+}
 
 void *ltbl_new(t_symbol *s, int argc, t_atom *argv)
 {
   
   t_ltbl *x = (t_ltbl *)pd_new(ltbl_class);
+  x->matrix = NULL;
   
-  // ltbl_init(x, s, argc, argv);
+  ltbl_init(x, s, argc, argv);
   
   return (void *)x;
 }
@@ -100,57 +187,9 @@ void ltbl_setup(void) {
         0);
   class_addmethod(ltbl_class, (t_method)ltbl_init, gensym("init"), A_GIMME, 0);
   class_addlist(ltbl_class, (t_method)ltbl_list);
+  class_addmethod(ltbl_class, (t_method)ltbl_destroy, gensym("destroy"), 0);
+  class_addmethod(ltbl_class, (t_method)ltbl_clear, gensym("clear"), 0);
+  class_addmethod(ltbl_class, (t_method)ltbl_test, gensym("test"), 0);
 }
 
-
-int main(int argc, char **argv) {
-  struct RGBLedMatrixOptions options;
-  struct RGBLedMatrix *matrix;
-  struct LedCanvas *offscreen_canvas;
-  int width, height;
-  int x, y, i;
-
-  memset(&options, 0, sizeof(options));
-  options.rows = 32;
-  options.chain_length = 1;
-
-  /* This supports all the led commandline options. Try --led-help */
-  matrix = led_matrix_create_from_options(&options, &argc, &argv);
-  if (matrix == NULL)
-    return 1;
-
-  /* Let's do an example with double-buffering. We create one extra
-   * buffer onto which we draw, which is then swapped on each refresh.
-   * This is typically a good aproach for animations and such.
-   */
-  offscreen_canvas = led_matrix_create_offscreen_canvas(matrix);
-
-  led_canvas_get_size(offscreen_canvas, &width, &height);
-
-  fprintf(stderr, "Size: %dx%d. Hardware gpio mapping: %s\n",
-          width, height, options.hardware_mapping);
-
-  for (i = 0; i < 1000; ++i) {
-    for (y = 0; y < height; ++y) {
-      for (x = 0; x < width; ++x) {
-        led_canvas_set_pixel(offscreen_canvas, x, y, i & 0xff, x, y);
-      }
-    }
-
-    /* Now, we swap the canvas. We give swap_on_vsync the buffer we
-     * just have drawn into, and wait until the next vsync happens.
-     * we get back the unused buffer to which we'll draw in the next
-     * iteration.
-     */
-    offscreen_canvas = led_matrix_swap_on_vsync(matrix, offscreen_canvas);
-  }
-
-  /*
-   * Make sure to always call led_matrix_delete() in the end to reset the
-   * display. Installing signal handlers for defined exit is a good idea.
-   */
-  led_matrix_delete(matrix);
-
-  return 0;
-}
   
